e415c7ed8f08d54ab09b386b1cc3c10ebdfe238e
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2023-09-20 20:10:47 +0530

    DeletionReverse ReverseDLL

---
+++ LinkedList/DeletionAndReverse.java
@@ -0,0 +1,38 @@
+package LinkedList;
+
+// class GFG
+// {
+//     //This method returns the head of the LL after deleting node with value d.
+//     public static Node deleteNode(Node head,int d)
+//     {
+//         //Add your code here.
+//         Node currNode = head;
+//         Node tobeDeleted = new Node(d);
+//         while(currNode.next.data != tobeDeleted.data){
+//             currNode = currNode.next;
+//         }
+//         currNode.next = currNode.next.next;
+//         return head;
+//     }
+//     //This method returns the head node of the reversed Linked list.
+//     public static Node reverse(Node head)
+//     {
+//         //Add your code here
+//         Node currNode = head;
+//         Node prev = null;
+//         do{
+//             Node next = currNode.next;
+//             currNode.next = prev;
+//             prev = currNode;
+//             currNode = next;
+//         }while (currNode != head);
+        
+//         currNode.next = prev;
+//         head = prev;
+//         return head;
+//     }
+// }
+
+public class DeletionAndReverse {
+    
+}
---
+++ LinkedList/ReverseADLL.java
@@ -0,0 +1,21 @@
+package LinkedList;
+
+public class ReverseADLL {
+    // public static Node reverseDLL(Node  head)
+    // {
+    //     //Your code 
+    //     Node currNode = head;
+    //     Node prev = null;
+    //     while (currNode != null){
+    //         Node next = currNode.next;
+    //         currNode.next = prev;
+    //         currNode.prev = next;
+    //         prev = currNode;
+    //         currNode = next;
+    //     }
+    //     return prev;
+    // }
+    public static void main(String[] args) {
+        
+    }
+}
