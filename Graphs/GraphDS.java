c8c048ecd803a0cb04a3c03e0b8f053cdf23c6d7
Neeraj Aloney <neeraj.aloney2001@gmail.com>
2023-12-25 14:57:54 +0530

    added bfs traversal method

--- Graphs/GraphDS.java
+++ Graphs/GraphDS.java
@@ -20,27 +20,61 @@ public class GraphDS {
             graph[i] = new ArrayList<Edge>();
         }
 
+        graph[0].add(new Edge(0, 1, 2));
         graph[0].add(new Edge(0, 2, 2));
 
-        graph[1].add(new Edge(1, 2, 10));
+        graph[1].add(new Edge(1, 0, 10));
         graph[1].add(new Edge(1, 3, 0));
 
-        graph[2].add(new Edge(2, 0,2));
-        graph[2].add(new Edge(2, 1, 10));
-        graph[2].add(new Edge(2, 3, -1));
+        graph[2].add(new Edge(2, 0, 2));
+        graph[2].add(new Edge(2, 4, 10));
+        // graph[2].add(new Edge(2, 3, -1));
 
+        graph[3].add(new Edge(3, 1, 5));
         graph[3].add(new Edge(3, 1, 0));
         graph[3].add(new Edge(3, 2, -1));
+
+        graph[4].add(new Edge(4, 2, 5));
+        graph[4].add(new Edge(4, 3, 0));
+        graph[4].add(new Edge(4, 5, -1));
+
+        graph[5].add(new Edge(5, 3, 5));
+        graph[5].add(new Edge(5, 4, 0));
+        graph[5].add(new Edge(5, 6, -1));
+        graph[5].add(new Edge(6, 5, 7));
     }
     
+    public static void bfs(ArrayList<Edge> graph[], int V) {
+        Queue<Integer> q = new LinkedList<>();
+        boolean[] visited = new boolean[V];
+        q.add(0);
+
+        while (!q.isEmpty()) {
+            int curr = q.poll();
+            if (!visited[curr]) {
+
+                System.out.print(curr + " ");
+                visited[curr] = true;
+
+                for (int i = 0; i < graph[curr].size(); i++) {
+                    Edge e = graph[curr].get(i);
+                    q.add(e.destination);
+                }
+
+            }
+        }
+    }
     public static void main(String[] args) {
-        int V = 4;
+        int V = 7;
         ArrayList<Edge> graph[] = new ArrayList[V];
         createGraph(graph);
 
-        for (int i = 0; i < graph[2].size(); i++) {
-            Edge e = graph[2].get(i);
-            System.out.println(e.destination+" "+e.weight);
-        }
+        // for (int i = 0; i < graph[2].size(); i++) {
+        //     Edge e = graph[2].get(i);
+        //     System.out.println(e.destination + " " + e.weight);
+        // }
+        bfs(graph, V);
+        System.out.println();
+
     }
 }
